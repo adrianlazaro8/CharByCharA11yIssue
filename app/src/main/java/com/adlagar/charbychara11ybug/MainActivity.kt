92500c2149cc936b75160c626e60d9d48c073e52
Adrian Lazaro <adri92css@gmail.com>
2024-08-05 11:15:11 +0200

    Texts added to the view

--- app/src/main/java/com/adlagar/charbychara11ybug/MainActivity.kt
+++ app/src/main/java/com/adlagar/charbychara11ybug/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -20,10 +21,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             CharByCharA11yBugTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Column(
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        Text(text = "Cancelled")
+                        Text(text = "CANCELLED")
+                    }
                 }
             }
         }
